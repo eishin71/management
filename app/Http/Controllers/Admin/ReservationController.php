14e76c03fd0d2367142efffb74ccb7e0edf0a5be
eishin <eshinkun71@gmail.com>
2020-12-17 20:54:11 +0900

    予約フォーム　前後1時間のエラー表示

--- app/Http/Controllers/Admin/ReservationController.php
+++ app/Http/Controllers/Admin/ReservationController.php
@@ -30,10 +30,8 @@ class ReservationController extends Controller
     $before_date = $before_date->subHour();
     //　$date=17:00だったら
     // reservationテーブルから17:00~17:59までのレコードを取り出す
-    $have_reservation = Reservation::where('date','>=',$date)
-                                   ->where('date','<',$after_date)
-                                   ->where('date','<=',$date)
-                                   ->where('date','>',$before_date)
+    $have_reservation = Reservation::where('date','<',$after_date)
+                                   ->where('date','>=',$before_date)
                                    ->exists();
 //confirmのcontrollerにcreatecontrollerと似たような処理を書く
 //確認画面でエラー画面を出せるようにする
@@ -133,17 +131,15 @@ class ReservationController extends Controller
       $before_date = $before_date->subHour();
       //　$date=17:00だったら
       // reservationテーブルから17:00~17:59までのレコードを取り出す
-      $have_reservation = Reservation::where('date','>=',$date)
-                                     ->where('date','<',$after_date)
-                                     ->where('date','<=',$date)
-                                     ->where('date','>',$before_date)
+      $have_reservation = Reservation::where('date','<',$after_date)
+                                     ->where('date','>=',$before_date)
                                      ->exists();
   //confirmのcontrollerにcreatecontrollerと似たような処理を書く
   //確認画面でエラー画面を出せるようにする
       //exists = true,falseを返す
       if ($have_reservation) {
             $error_message = 'この時間はすでに予約が入っています';
-            return view('admin.reservation.confirm',['date' => $date, 'error_message' => $error_message,'courses' => $courses]);
+            return view('admin.reservation.create',['date' => $date, 'error_message' => $error_message,'courses' => $courses]);
       } else {
       return view('admin.reservation.confirm',['form' => $form]);
        }
