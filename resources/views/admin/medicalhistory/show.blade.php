ffcfe6fc2b4012dd2a5e379c54bcd586b4805fb0
eishin <eshinkun71@gmail.com>
2021-02-24 19:50:35 +0900

    show戻るボタンの修正

--- resources/views/admin/medicalhistory/show.blade.php
+++ resources/views/admin/medicalhistory/show.blade.php
@@ -10,7 +10,7 @@
     </div>
     <div class="row">
         <div class="col-md-4">
-            <a href="{{ action('Admin\MedicalHistoryController@index',['id' => $client->id]) }}">戻る</a>
+            <a href="{{ action('Admin\MedicalHistoryController@index',['id' => $client_id]) }}">戻る</a>
         </div>
     </div>
     <h2>問診票</h2>
